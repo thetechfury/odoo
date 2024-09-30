b55cf11877ed6c3707160009befc16a26c95b6b2
Moeez ali khan <moeez4@gmail.com>
2024-09-30 13:51:11 +0500

    deployment file added

---
+++ deployment.sh
@@ -0,0 +1,13 @@
+#Deployment File
+BASE_PATH="/root/web/odoo"
+cd ${BASE_PATH}
+#activate Virtual Env
+source odoo-17-env/bin/activate/
+# Pull Source code
+git pull
+
+# Install python required files
+pip install -r ${BASE_PATH}/requirements.txt
+
+# RUN APPLICATION
+python odoo-bin -c "/root/web/odoo/conf/odoo.prod.conf" -d odoottf
